e84c22c18f92588dd9a560b77cb1ac83c41dac0b
Alextibtab <14alexcockburn@gmail.com>
2024-09-23 02:02:37 +0100

    more comments

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -6,6 +6,7 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
+    // TODO: figure out why my coordinate system isn't properly working
     // gl_Position = projection * view * model * vec4(position, 1.0);
     gl_Position = vec4(position, 1.0);
 }
